9813a53734e0f1f646547a44a3305c36949e8917
BirdyCoder <349281162@qq.com>
2018-12-11 07:00:14 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/learnGit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learnGit.dir/main.cpp.o"
+  "learnGit.pdb"
+  "learnGit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learnGit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
