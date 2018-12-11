aefa6c1dcdd8752d3fa01217803654d96494fa47
BirdyCoder <349281162@qq.com>
2018-12-11 07:22:11 +0100

    second

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/liuzhuofan/Desktop/workplace/learnGit
+# Install script for directory: /Users/liuzhuofan/Desktop/learnGit
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/liuzhuofan/Desktop/workplace/learnGit/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/liuzhuofan/Desktop/learnGit/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
